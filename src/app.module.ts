a0d90326f18632bcad512eead84ecd9c4c7a2170
ali-mansouri-b <ali.mansouribot@gmail.com>
2025-05-17 06:32:11 +0330

    feat: add a new controller corgi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CorgiController } from './corgi/corgi.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CorgiController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/corgi/corgi.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Render } from '@nestjs/common';
+
+@Controller('corgi')
+export class CorgiController {
+  @Get()
+  @Render('index')
+  showCorgis() {
+    return { message: 'Corgis are the best!' };
+  }
+}
